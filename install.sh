29b4dd3ad95aaed66737ff0101b7ec7770f09922
tatsuya <user@tatsuya.local>
2017-08-13 21:57:42 +0900

    シンボリックリンクの元を実行シェルの絶対パスに変更

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+DIR=$(cd $(dirname $0); pwd)
+
 for f in .??*
 do
     [[ "$f" == ".git" ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
 
-    ln -sf ~/dotfiles/$f ~/$f
+    ln -sf $DIR/$f ~/$f
 done
