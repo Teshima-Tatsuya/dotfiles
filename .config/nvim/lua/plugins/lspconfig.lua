0d22b0528802bc58ea36bc3ac0f25d85fbef68f4
Tatsuya Teshima <terima666tatsuya@gmail.com>
2025-07-12 13:36:01 +0900

    fix to neovimv0.11

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -4,59 +4,22 @@ return {
 		"hrsh7th/cmp-nvim-lsp",
 	},
 	config = function()
-		local lspconfig = require("lspconfig")
-		local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
-		-- Add completion capabilities from nvim-cmp
-		local capabilities = cmp_nvim_lsp.default_capabilities()
-
-		-- Configure language servers here
-		-- Example for lua_ls (Lua Language Server)
-		lspconfig.lua_ls.setup({
-			capabilities = capabilities,
-			settings = {
-				Lua = {
-					runtime = {
-						version = "LuaJIT",
-					},
-					diagnostics = {
-						globals = { "vim" },
-					},
-					workspace = {
-						library = vim.api.nvim_get_runtime_file("", true),
-					},
-					telemetry = {
-						enable = false,
-					},
-				},
-			},
+		-- Set global capabilities for all LSP servers
+		vim.lsp.config('*', {
+			capabilities = require('cmp_nvim_lsp').default_capabilities(),
 		})
 
-		-- Rust
-		lspconfig.rust_analyzer.setup({
-			capabilities = capabilities,
-			settings = {
-				["rust-analyzer"] = {
-					checkOnSave = {
-						command = "clippy",
-					},
-				},
-			},
-		})
+		-- Enable LSP servers
+		local servers = {
+			'lua_ls',
+			'rust_analyzer', 
+			'gopls',
+			'ts_ls',
+			'bashls',
+		}
 
-		-- Go
-		lspconfig.gopls.setup({
-			capabilities = capabilities,
-		})
-
-		-- TypeScript/JavaScript
-		lspconfig.ts_ls.setup({
-			capabilities = capabilities,
-		})
-
-		-- Bash
-		lspconfig.bashls.setup({
-			capabilities = capabilities,
-		})
+		for _, server in ipairs(servers) do
+			vim.lsp.enable(server)
+		end
 	end,
 }
