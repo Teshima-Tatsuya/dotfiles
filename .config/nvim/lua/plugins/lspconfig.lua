2bdf7e9de25013fb8b26bd4b93502109053d0b13
Tatsuya Teshima <terima666tatsuya@gmail.com>
2025-12-07 10:24:43 +0900

    global vim

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,25 +1,36 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	config = function()
-		-- Set global capabilities for all LSP servers
-		vim.lsp.config('*', {
-			capabilities = require('cmp_nvim_lsp').default_capabilities(),
-		})
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+        -- Set global capabilities for all LSP servers
+        vim.lsp.config('*', {
+            capabilities = require('cmp_nvim_lsp').default_capabilities(),
+        })
 
-		-- Enable LSP servers
-		local servers = {
-			'lua_ls',
-			'rust_analyzer', 
-			'gopls',
-			'ts_ls',
-			'bashls',
-		}
+        vim.lsp.config('lua_ls', {
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { 'vim' },
+                    },
+                },
+            },
+        })
 
-		for _, server in ipairs(servers) do
-			vim.lsp.enable(server)
-		end
-	end,
+        -- Enable LSP servers
+        local servers = {
+            'lua_ls',
+            'rust_analyzer',
+            'gopls',
+            'ts_ls',
+            'bashls',
+        }
+
+        for _, server in ipairs(servers) do
+            vim.lsp.enable(server)
+        end
+    end,
 }
+
