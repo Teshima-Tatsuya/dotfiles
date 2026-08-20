c647dca96a79f0650cb711ff67139ab3796d422a
Tatsuya Teshima <terima666tatsuya@gmail.com>
2026-08-20 09:54:29 +0900

    remove enableZshIntegration

--- nix/home/zellij.nix
+++ nix/home/zellij.nix
@@ -3,7 +3,6 @@
 {
     programs.zellij = {
         enable = true;
-        enableZshIntegration = true;
     };
 
     xdg.configFile."zellij/config.kdl".source = "${self}/.config/zellij/config.kdl";
